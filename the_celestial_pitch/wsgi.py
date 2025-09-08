d4c0053ab3f0f82cade7b2f29d753cad21a68521
SeeStarz <seestarz@tutamail.com>
2025-09-08 10:01:28 +0700

    Start a new django project

---
+++ the_celestial_pitch/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for the_celestial_pitch project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_celestial_pitch.settings')
+
+application = get_wsgi_application()
