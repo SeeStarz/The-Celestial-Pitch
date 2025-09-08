15fd58b15f54067044560e0a2dba6fe765121b1c
SeeStarz <seestarz@tutamail.com>
2025-09-08 10:38:49 +0700

    Add item model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    CATEGORY_CHOICES = [
+        ('gear', 'Gear'),
+        ('relic', 'Relic'),
+        ('consumable', 'Consumable'),
+        ('powerup', 'Power Up'),
+        ('special', 'Special'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    base_price = models.IntegerField()
+    description = models.TextField()
+    icon = models.URLField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=31)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
