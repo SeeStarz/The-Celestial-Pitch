74970aae674110a4898bb78a4fdeaccd78a2b0f9
SeeStarz <seestarz@tutamail.com>
2025-09-15 21:52:59 +0700

    Actually sync to main

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.conf import settings
 import main.views as views
 
 app_name = 'main'
 
 urlpatterns = [
     path('', views.show_index, name='index'),
-    path('static/<path:name>', views.show_static, name='static'),
+    # We remove prefix because for some reason django prepends slash and it breaks this
+    path(settings.STATIC_URL.lstrip('/') + '<path:name>', views.show_static, name='static'),
 ]
--- the_celestial_pitch/settings.py
+++ the_celestial_pitch/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 STATIC_URL = 'static/'
-STATIC_ROOT = os.getenv('STATIC_ROOT', 'static_root')
+STATIC_ROOT = os.getenv('STATIC_ROOT', BASE_DIR / 'static_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
